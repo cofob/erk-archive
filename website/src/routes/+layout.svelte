cef614cc228aad5eb7c98e2f5cddea7a9a12edf7
Egor Ternovoy <cofob@riseup.net>
2022-10-21 00:48:27 +0700

    Remove old useless web blocks

--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -7,25 +7,15 @@
 	<link rel="preconnect" href={import.meta.env.VITE_IPFS_ENDPOINT} />
 </svelte:head>
 
-<!-- Background -->
-<div class="min-h-screen min-w-screen bg-neutral-800 text-neutral-200">
-	<!-- App -->
-	<app class="hidden lg:block" data-sveltekit-prefetch>
-		<div class="absolute top-0 left-0" id="modal-zone" />
-		<slot />
-	</app>
-
-	<!-- Mobile stub -->
-	<div class="block lg:hidden text-center py-10">
-		<Heading level={4}>Слишком маленький экран</Heading>
-		<Heading level={5} element="p">Зайдите с компьютера</Heading>
+<noscript>
+	<div class="text-center py-10">
+		<Heading level={1}>Включите JS!</Heading>
+		<p>Без него сайт не будет работать.</p>
 	</div>
+</noscript>
 
-	<!-- Noscript error -->
-	<noscript>
-		<div class="text-center py-10">
-			<Heading level={1}>Включите JS!</Heading>
-			<p>Без него сайт не будет работать.</p>
-		</div>
-	</noscript>
-</div>
+<div class="absolute top-0 left-0" id="modal-zone" />
+
+<main data-sveltekit-prefetch>
+	<slot />
+</main>
